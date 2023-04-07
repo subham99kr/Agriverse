6d010cf04e6b16f64fdb78408dc115e3e8bc022a
sri8397 <[srikant9004@gmail.com]>
2023-04-08 00:15:42 +0530

    new file:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/assets/logo-bold.png 	modified:   src/components/Header.js 	new file:   src/styles/fonts.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
     <>
-      Hello
+      <Router>
+        <Routes>
+          <Route path='/' element={<><Header/></>} />
+        </Routes>
+      </Router>
     </>
   );
 }
