45c0e95ae4109a40c2a6a8e9444c48ff874be957
sri8397 <[srikant9004@gmail.com]>
2023-04-08 06:18:29 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/Firebase.js 	modified:   src/components/Imagecarousel.js 	deleted:    src/pages/Products.js 	new file:   yarn.lock

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,22 +1,19 @@
-import 'firebase/auth';
-import 'dotenv'
+import "firebase/auth";
 
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "hackhack-db78d.firebaseapp.com",
-  projectId: "hackhack-db78d",
-  storageBucket: "hackhack-db78d.appspot.com",
-  messagingSenderId: "535749067895",
-  appId: "1:535749067895:web:4c050e4c7cce6842e77272",
-  measurementId: "G-91YWELK959"
+  apiKey: "AIzaSyBm5VJK7bvh18rpt4CM9wiOwuLZJNu3lfM",
+  authDomain: "webgriphunters-117f8.firebaseapp.com",
+  projectId: "webgriphunters-117f8",
+  storageBucket: "webgriphunters-117f8.appspot.com",
+  messagingSenderId: "500284906679",
+  appId: "1:500284906679:web:4ad631b869a47809990132",
+  measurementId: "G-7D1JR98FWQ"
 };
-
-// Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-export { auth, db }; 
+export { auth, db };
--- src/components/Imagecarousel.js
+++ src/components/Imagecarousel.js
@@ -1,30 +1,23 @@
-import '../styles/fonts.css'
+import { useEffect, useState } from 'react';
+import '../styles/fonts.css';
+import { db } from '../Firebase';
+import { collection, getDocs } from "firebase/firestore"
+
+
 const ImageCarousel = () => {
-  let products = [
-    {
-      id: 1,
-      name: "Nagpuri Oranges",
-      href: "#",
-      imageSrc: "https://images.slurrp.com/prod/articles/pue8orx8te.webp",
-      imageAlt: "Best Oranges in India",
-      price: "INR 120/KG ",
-    },
-    {
-      id: 2,
-      name: "Kashmiri Apples",
-      href: "#",
-      imageSrc:
-        "https://images.news18.com/ibnlive/uploads/2021/11/apple-1-16361875623x2.jpg?impolicy=website&width=510&height=356",
-      imageAlt: "Best Apples in India",
-      price: "INR 80/KG",
-    },
-  ];
-  for (let i = 0; i < 2; i++) {
-    products.push(...products);
-  }
-  const styles = {
-    backgroundColor: "#ffffef",
-  };
+  const [product, setProduct] = useState([]);
+  const userCollectionRef = collection(db, "products");
+  useEffect(() => {
+    const getProducts = async () => {
+      const data = await getDocs(userCollectionRef)
+      console.log(data)
+      setProduct(data.docs.map((doc) =>({ ...doc.data(), id: doc.id })))
+      console.log(product)
+    }
+    getProducts(); 
+  }, []);
+
+
   return (
     <swiper-container
       grabCursor={true}
@@ -32,24 +25,17 @@ const ImageCarousel = () => {
       slides-per-view="auto"
       navigation="true"
       pagination={true}
-      style={styles}
+      style={{backgroundColor: "#ffffee"}}
     >
-      {products.map((items) => {
+      {product.map((items) => {
         return (
           <swiper-slide lazy="true">
             <div className="md:flex">
               <div className="md:basis-1/2 basis-1">
-                <img src="https://picsum.photos/700/500" alt={items.imageAlt} loading="lazy" />
+                <img src={items.imageUrl} alt={items.imageAlt} loading="lazy" />
               </div>
               <div className="md:basis-1/2 basis-1">
-                <p className="text-center text-xl font-montserrat ">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-                  sit amet quam leo. Curabitur imperdiet ligula vel convallis
-                  auctor. Fusce interdum, tortor sit amet mollis ultricies,
-                  risus libero blandit neque, ut luctus elit tortor a ante. Nam
-                  nec lorem risus. Fusce condimentum nibh non risus venenatis
-                  rutrum quis finibus neque.
-                </p>
+                <p className="text-center text-xl font-montserrat ">{items.description}</p>
               </div>
             </div>
           </swiper-slide>
